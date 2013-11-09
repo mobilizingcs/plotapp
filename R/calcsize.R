ce3e152581797df2adfba91fa38a65f2dfc32365
Jeroen Ooms <jeroenooms@gmail.com>
2013-11-08 16:28:53 -0800

    cleanup

--- R/calcsize.R
+++
@@ -1,5 +0,0 @@
-calcsize <- function(x, bins=30){
-  binwidth <- diff(range(x)) / bins;
-  peak <- max(table(round(x/binwidth)))
-  return(bins/peak/1.8);
-}
--- R/makeplot_dotplot.R
+++
@@ -1,4 +0,0 @@
-makeplot_dotplot <- function(data, x, ...){
-  myplot <- do.call("qplot", c(as.list(match.call())[-1], geom="blank"));
-  myplot + geom_dotplot();
-}
