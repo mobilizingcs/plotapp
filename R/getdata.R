91f9dffe445a1aa45831253bb0db6839e44e0124
Jeroen Ooms <jeroenooms@gmail.com>
2013-11-18 17:15:16 -0800

    adding 'privacy' variable

--- R/getdata.R
+++ R/getdata.R
@@ -9,7 +9,8 @@
 #' @importFrom Ohmage oh.survey_response.read
 #' @export
 getdata <- function(campaign_urn, serverurl, token, ...){
-  mydata <- oh.survey_response.read(campaign_urn, serverurl=serverurl,token=token, ...);
+  mydata <- oh.survey_response.read(campaign_urn, serverurl=serverurl,token=token, 
+    column_list = "urn:ohmage:user:id,urn:ohmage:prompt:response,urn:ohmage:context:timestamp,urn:ohmage:survey:privacy_state", ...);
   if(nrow(mydata) == 0){
     stop("No survey responses found for this campaign/range.")
   }
@@ -17,6 +18,7 @@ getdata <- function(campaign_urn, serverurl, token, ...){
   mydata$date <- as.Date(mydata$context.timestamp);
   mydata$timestamp <- as.POSIXct(mydata$context.timestamp);
   mydata$user <- as.factor(mydata$user.id);
+  mydata$privacy <- as.factor(mydata$survey.privacy_state);
   
   #some fixes
   for(i in seq_along(mydata)){
--- inst/www/js/app.js
+++ inst/www/js/app.js
@@ -59,11 +59,11 @@ $(function() {
   function populatevars(){
     var mydata = campaigndata[campaign_urn];
     var surveyid = $("#surveyfield").val();
-    $("#xfield").empty().append($("<option>").text("date")).append($("<option>").text("user"));
-    $("#yfield").empty().append($("<option>"));
-    $("#colorfield").empty().append($("<option>")).append($("<option>").text("user"));
-    $("#sizefield").empty().append($("<option>"));   
-    $("#facetfield").empty().append($("<option>")).append($("<option>").text("user"));     
+    $("#xfield").empty().append($("<option>").text("date")).append($("<option>").text("user")).append($("<option>").text("privacy"));
+    $("#yfield").empty().append($("<option>").val("").text("response count"));
+    $("#colorfield").empty().append($("<option>").val("").text("—")).append($("<option>").text("user")).append($("<option>").text("privacy"));
+    $("#sizefield").empty().append($("<option>").val("").text("—"));   
+    $("#facetfield").empty().append($("<option>").val("").text("—")).append($("<option>").text("user")).append($("<option>").text("privacy"));     
     $.each(mydata[surveyid].prompts, function(i, val){
       if(val.promptType == "text" || val.promptType == "photo") return;
       $("#xfield").append($("<option>").val(val.id).text(val.id)); //.text(val.promptlabel)); 
