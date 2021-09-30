ce1aca2c51c747cdfa0a7c9af0929f079514d046
sKayastha95 <kayasthashubham95@gmail.com>
2021-09-30 12:11:05 -0500

    Update fitline.R

--- R/fitline.R
+++ R/fitline.R
@@ -1,12 +1,12 @@
 fitline <- function(type = c("linear", "quadratic", "cubic", "exponential", "log", "loess"), se = FALSE){
 	type <- match.arg(type);
 	switch(type,
-		"linear" = geom_smooth(method="glm", colour="navy", fill=NA, group=NA, se = se),
-		"quadratic" = geom_smooth(method="glm", colour="navy", fill=NA, group=NA, formula = y~poly(x,2), se = se),
-		"cubic" = geom_smooth(method="glm", colour="navy", fill=NA, group=NA, formula = y~poly(x,3), se = se),
-		"exponential" = geom_smooth(method="glm", colour="navy", fill=NA, group=NA, formula = y~x, se = se, family = gaussian(link = 'log')),
-		"log" = geom_smooth(method="glm", colour="navy", fill=NA, group=NA, formula = y~log(x), se = se),
-		"loess" = geom_smooth(colour="navy", fill=NA, group=NA, se = se),
+		"linear" = geom_smooth(method="glm", colour="navy", se = se),
+		"quadratic" = geom_smooth(method="glm", colour="navy", formula = y~poly(x,2), se = se),
+		"cubic" = geom_smooth(method="glm", colour="navy", formula = y~poly(x,3), se = se),
+		"exponential" = geom_smooth(method="glm", colour="navy", formula = y~x, se = se, family = gaussian(link = 'log')),
+		"log" = geom_smooth(method="glm", colour="navy", formula = y~log(x), se = se),
+		"loess" = geom_smooth(colour="navy", se = se),
 		stop("unknown smooth type")
 	)
 }
